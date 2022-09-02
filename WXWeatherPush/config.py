baa65b579afc4813756b54a8397e9c9b962a1819
“xiaoliu006” <774480557@qq.com>
2022-09-02 14:57:30 +0800

    TQshiyan

---
+++ WXWeatherPush/config.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+# 公众号配置
+# 公众号appId
+app_id = "wxf13b33111cf06500"
+# 公众号appSecret
+app_secret = "c72818b9958f5a6e8b0b15ab9e90e901"
+# 考研时间，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+kaoyanday = "2021-12-24"
+
+# 信息配置
+userList=[{  #第一个人
+        'user':"odvo16Ou6edd-SG7WupoRj2Y_49o", #微信号
+        'province':"新疆", # 所在省份
+        'city':"乌鲁木齐", # 所在城市
+        'birthday':"2000-1-10",# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+        'birthday1':"1999-2-8",  # 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+        'love_date':"2018-12-2",# 在一起的日子，格式同上
+        'template_id':"2KXLCaw8wHuHb0wql5g0euFBgByLFozmgzMN9mv6TTc"  #模板号
+    },{  #第一个人
+        'user':"odvo16HbNHrtKZHLzqpTey8l4mYU", #微信号
+        'province':"江西", # 所在省份
+        'city':"吉安", # 所在城市
+        'birthday':"1999-2-8",# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+        'birthday1':"2000-1-10",# 生日，如果月份或者日期小于10，直接用对应的数字即可，例如1997-1-1
+        'love_date':"2018-12-2",# 在一起的日子，格式同上
+        'template_id':"QToP7SgZkf3zhCbPE3eg2ld8zwf0fwPDnNYCy4-qwFI"  #模板号
+    }]
+ 
